467f6d20cd5d2532de80a655c7d688bbc821ad5c
Maxime Bourreau <maximegb@gmail.com>
2017-11-02 20:57:31 +0100

    Added confirmation of deleting a game

--- src/GameCard.js
+++ src/GameCard.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Confirm } from 'semantic-ui-react';
 
-export default function GameCard({ game, deleteGame }) {
-  return (
-    <div className="ui card">
-      <div className="image">
-        <img src={game.cover} alt="Game Cover" />
-      </div>
-      <div className="content">
-        <div className="header">{game.title}</div>
-      </div>
-      <div className="extra content">
-        <div className="ui two buttons">
-          <Link to={`/game/${game._id}`} className="ui basic button green">Edit</Link>
-          <div className="ui basic button red" onClick={() => deleteGame(game._id)}>Delete</div>
+class GameCard extends React.Component {
+  state = {
+    confirmDelete: false
+  }
+  render() {
+    return (
+      <div className="ui card">
+        <div className="image">
+          <img src={this.props.game.cover} alt="Game Cover" />
         </div>
+        <div className="content">
+          <div className="header">{this.props.game.title}</div>
+        </div>
+        <div className="extra content">
+          <div className="ui two buttons">
+            <Link to={`/game/${this.props.game._id}`} className="ui basic button green">Edit</Link>
+            <div className="ui basic button red" onClick={() => { this.setState({ confirmDelete: true }); }}>Delete</div>
+          </div>
+        </div>
+        <Confirm
+          content = {`Delete ${this.props.game.title} ?`}
+          confirmButton = {'Delete'}
+          open={ this.state.confirmDelete }
+          onCancel={() => { this.setState({ confirmDelete: false }); }}
+          onConfirm={() => this.props.deleteGame(this.props.game._id) }
+        />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 GameCard.propTypes = {
   game: PropTypes.object.isRequired,
   deleteGame: PropTypes.func.isRequired
 }
+
+export default GameCard;
